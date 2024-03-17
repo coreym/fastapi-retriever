9d58c0fb6772c321a43d891c4bdd11059469043a
Corey Maher <corey.maher@gmail.com>
2024-03-17 01:28:55 -0400

    moved retriever to utils, added query route to main.py

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-from typing import Optional
-
 from fastapi import FastAPI
+from typing import Optional
+from openai import OpenAI
+from pinecone import Pinecone
+import utils.retriever as retriever
+import os
 
+PINECONE_API_KEY = os.getenv('PINECONE_KEY')
+OPENAI_API_KEY = os.getenv('OPENAI_KEY')
+
+pc = Pinecone(api_key=PINECONE_API_KEY)
+index = pc.Index("testindex")
+openai_client = OpenAI(api_key=OPENAI_API_KEY)
 app = FastAPI()
 
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Optional[str] = None):
+#     return {"item_id": item_id, "q": q}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/search/{query}")
+def search(query: str):
+    embed = retriever.get_embedding(openai_client,query)
+    results = retriever.get_matches(index,embed,namespace='servicecloud')
+    return results.to_dict()
--- utils/retriever.py
+++ utils/retriever.py
@@ -1,15 +1,5 @@
-import os
-from fastapi import FastAPI
-from openai import OpenAI
-from pinecone import Pinecone
-PINECONE_API_KEY = os.getenv('PINECONE_KEY')
-OPENAI_API_KEY = os.getenv('OPENAI_KEY')
-pc = Pinecone(api_key=PINECONE_API_KEY)
-index = pc.Index("pinecone-index")
-openai_client = OpenAI(api_key=OPENAI_API_KEY)
-
-# function to generate vector embeddings with the OpenAI text-embedding-3-small model
-def get_embedding(text):
+def get_embedding(openai_client, text):
+    """ function to generate vector embeddings with the OpenAI text-embedding-3-small model"""
     # generate vector embeddings for the text data
     response = openai_client.embeddings.create(
     model="text-embedding-3-small",
@@ -30,11 +20,11 @@ def get_matches(pinecone_client,query_vector,top_k=3,**kwargs):
     response = pinecone_client.query(**clientargs)
     return response
 # def format_for_gpt(ra)
-app = FastAPI()
-@app.get("/")
-def root():
-    return {"message": "Hello World!"}
+# app = FastAPI()
+# @app.get("/")
+# def root():
+#     return {"message": "Hello World!"}
 
-@app.get("/search/{query}")
-def search(query: str):
-    return f"Searching for {query}..."
+# @app.get("/search/{query}")
+# def search(query: str):
+#     return f"Searching for {query}..."
